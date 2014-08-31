e48752ac49a9152bbc3326470dbefa939ccd3f6b
Mark Andrew <mark.andrew@gmail.com>
2014-08-31 11:11:57 +0200

    Initial Commit

---
+++ Starter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Starter
+//
+//  Created by mark andrew on 31/08/14.
+//  Copyright (c) 2014 mark andrew enterprises inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
