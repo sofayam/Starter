912e3991dc5ef46bc789bcb00bc0131ad2049d21
Mark Andrew <mark.andrew@gmail.com>
2014-09-02 22:31:59 +0200

    more changes

--- Starter/Second.swift
+++ Starter/Second.swift
@@ -20,6 +20,19 @@ class Second: UIViewController {
     }
 
     
+    func getStuff() {
+        let url = NSURL(string: "http://www.stackoverflow.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) {(data, response, error) in
+            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        }
+        
+        task.resume()
+    }
+    
+    @IBAction func getURL(sender: AnyObject) {
+        getStuff()
+    }
     
     
     @IBAction func pressedIt(sender: AnyObject) {
